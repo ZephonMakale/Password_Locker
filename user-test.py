3adff38d3b095d9c42b60073614d187d5d6ef9e3
ZephonMakale <mzephon@gmail.com>
2020-11-20 12:55:34 +0300

    Add the first test

--- user-test.py
+++ user-test.py
@@ -1,8 +1,8 @@
-import unittest
-from user import User
+import unittest #Imports the unittest module.
+from user import User  #Imports the user class.
 
 
-class TestClass(unittest, TestCase):
+class TestUser(unittest, TestCase): # Creates a subclass known as TestUser.
     """
     Test class that defines test cases for the Users class behaviours.
 
@@ -15,5 +15,13 @@ class TestClass(unittest, TestCase):
         Set up method to run before each test cases.
         """
         self.new_users = User("Zephon Makale", "1234xyz") #Create User object
-    
+
+    def test_init(self):
+        """
+        test_init: test case to test if the object is initialized properly.
+        """
+
+        self.assertEqual(self.new_users.username, "Zephon Makale")
+        self.assertEqual(self.new_users.password, "1234xyz")
+        
     
