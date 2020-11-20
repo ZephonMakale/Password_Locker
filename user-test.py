14f3b727a2dd54483b0678cafab780e1840093ee
ZephonMakale <mzephon@gmail.com>
2020-11-20 12:44:05 +0300

    Add the setup method that runs before the test method run

--- user-test.py
+++ user-test.py
@@ -3,4 +3,17 @@ from user import User
 
 
 class TestClass(unittest, TestCase):
+    """
+    Test class that defines test cases for the Users class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases.
+    """
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_users = User("Zephon Makale", "1234xyz") #Create User object
+    
     
