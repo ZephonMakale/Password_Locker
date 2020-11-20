e8a8db8a97558f8890957cb8d695955dca938c04
ZephonMakale <mzephon@gmail.com>
2020-11-20 12:29:45 +0300

    Add a user class to the user.py file

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    """
+    Creates a user class that generates new intances of the User.
+    """
+
+    user_list[]
+
+    def __init__(self, username, password):
+
+        """
+        This function method gets to define the properties of the user. That's their username and password.
+        """
+
+        self.username = username
+        self.password = password
+    
+    
